d8c2361da83139e2abbc70b8cc25dcb7d055ddde
jhwgnu <jhwon11@snu.ac.kr>
2017-01-18 14:28:39 +0900

    create project

---
+++ board/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from board.models import School, Restaurant, Meal
+# Register your models here.
+
+admin.site.register(School)
+admin.site.register(Restaurant)
+admin.site.register(Meal)
---
+++ board/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class School(models.Model):
+    school_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.school_name
+
+class Restaurant(models.Model):
+    school = models.ForeignKey(School)
+    rest_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.rest_name
+
+class Meal(models.Model):
+    school = models.ForeignKey(School)
+    restaurant = models.ForeignKey(Restaurant)
+    meal_name = models.CharField(max_length=200)
+    price = models.IntegerField()
