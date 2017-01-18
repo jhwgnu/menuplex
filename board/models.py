44e4415f947a1b1f820f4724db77aaad1f456a1f
juyoungkim <juyoungkim@korea.ac.kr>
2017-01-18 15:18:52 +0900

    school db 속에 url field 추가하고, 서울대 html에서도 보기에 수정했음

--- board/models.py
+++ board/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class School(models.Model):
     school_name = models.CharField(max_length=200)
-
+    school_url = models.CharField(max_length=200)
     def __str__(self):
         return self.school_name
 
