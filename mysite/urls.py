905a74e1b626b175f6706fdbb2fa6b48f29708bc
juyoungkim <juyoungkim@korea.ac.kr>
2017-02-07 14:46:12 +0900

    url중에 localhost:/board/schoolshortcut -> localhost:/schoolshortcut으로 간소화시킴

--- mysite/urls.py
+++ mysite/urls.py
@@ -9,9 +9,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^board/', include('board.urls')),
     url(r'^$', views.index, name='index'),
-
+    url(r'^', include('board.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
